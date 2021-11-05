d83f6387e8903d1d30599184052f7be7406202a9
ruhulhit <91396640+ruhulhit@users.noreply.github.com>
2021-11-05 08:51:42 +0600

    test changes

---
+++ js/active.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		items:1,
+		mouseDrag:true,
+		autoplay:true,
+		loop:true,
+		center:true,
+		pullDrag:true,
+		autoplay:true,
+		rtl:true,
+	});
+  });
